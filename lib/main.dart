09ae1e294a1f77356308d59cd9aecea00b0923c2
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-02-18 22:11:58 +0530

    Signup is ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
-import 'intro_slider/intro_slider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gconnect/home/home.dart';
+import 'package:gconnect/intro_slider/intro_slider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); 
+  final pref = await SharedPreferences.getInstance();
+
+  if(pref.getBool('isUser') == true)
+  {
+    runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+  }
+  else
+  {
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
