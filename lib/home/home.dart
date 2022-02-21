c3e96d5a99372584fe2d30bbb234ca9cfcd1cc7a
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-02-21 16:03:25 +0530

    home page added

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,20 +1,98 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
-class HomePage extends StatefulWidget {
-const HomePage({Key? key}) : super(key: key);
+import 'package:icon_badge/icon_badge.dart';
 
-@override
-_HomePageState createState() => _HomePageState();
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-@override
-Widget build(BuildContext context) {
-	return Scaffold(
-	appBar: AppBar(
-		backgroundColor: Colors.green,
-		centerTitle: true,
-		title: const Text("G-Connect"),
-		
-	));
-}
+  int _currentIndex = 0;
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("G-Connect"),
+        backgroundColor: Colors.purple,
+        actions: <Widget>[
+          IconBadge(
+            icon: const Icon(Icons.notifications_none),
+            itemCount: 100,
+            badgeColor: Colors.red,
+            itemColor: Colors.white,
+            maxCount: 99,
+            hideZero: true,
+            onTap: () {
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.purple,
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        showElevation: true,
+        itemCornerRadius: 24,
+        curve: Curves.easeIn,
+        onItemSelected: (index) => setState(() => _currentIndex = index),
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            activeColor: Colors.black,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.favorite_border_outlined),
+            title: const Text('Favorites'),
+            activeColor: Colors.black,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.qr_code_2_outlined),
+            title: const Text(
+              'My QR',
+            ),
+            activeColor: Colors.black,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.account_box_outlined),
+            title: const Text('Profile'),
+            activeColor: Colors.black,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
 }
